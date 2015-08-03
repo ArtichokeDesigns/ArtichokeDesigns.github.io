e55dea28a8c3089df44f54d7650361c194c79c4f
Joe Biggica <joe.biggica@thomsonreuters.com>
2015-08-03 17:09:07 -0400

    added nav animation

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,20 @@
 $(document).ready(function(){
-  $('.your-class').slick({
-    setting-name: setting-value
-  });
+  // HEADER TRANSFORMER FUNCTION
+	$(window).scroll(function() {
+		var header = $('.nav-bar');
+		var scroll = $(window).scrollTop();
+
+        if (scroll >= 380) {
+            header.css({
+            	'height': '36px',
+            	'padding-top': '12px'
+            });  
+        } else {
+            header.css({
+            	'height': '50px',
+            	'padding-top': '24px'
+            });   
+        }
+
+	});
 });

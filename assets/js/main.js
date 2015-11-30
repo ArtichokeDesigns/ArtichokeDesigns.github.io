111b08dd7362af429e5cfd18d6ce4b6bb193885e
JoeBiggia <jbaki91@yahoo.com>
2015-11-30 13:05:46 -0500

    lazy load test

--- assets/js/main.js
+++ assets/js/main.js
@@ -136,16 +136,39 @@ $.each(moviePosters, function(k, v) {
 var allThumbs = $('.content-thumbs .content');
 var modalDataTarget;
 var modalImg;
-var modalContainer;
-for (i=0; i < allThumbs.length; i++) {
-    modalDataTarget = $(allThumbs[i]).attr('data-target');
+
+$(allThumbs).on("shown.bs.modal", function(){
+    modalDataTarget = $(this).attr('data-target');
     modalImgObj = $(modalDataTarget+'.modal');
-    $(allThumbs[i]).on("shown.bs.modal", function(){
-        modalImgObj.lazyload({
-            container:$(modalDataTarget)
-        });
+    modalImgObj.lazyload({
+        container:$(modalDataTarget)
     });
-}
+});
+
+
+// jQuery( function($) { 
+//     // find all tr's with a data-href attribute
+//     $(allThumbs).click( function() {
+//         // copy the data-href value to the modal for later use
+//         $('#modal-editUser').attr('data-href',$(this).attr('data-href'));
+//         // show the modal window
+//         $('#modal-editUser').modal({show: true , backdrop : true , keyboard: true});
+//     }).find('a').hover( function() { 
+//         // unbind it in case I put some a tags in the table row eventually
+//         $(this).parents('tr').unbind('click'); 
+//     }, function() { 
+//         $(this).parents('tr').click( function() { 
+//             // rebind it
+//             $('#modal-editUser').attr('data-href',$(this).attr('data-href'));
+//             $('#modal-editUser').modal({show: true , backdrop : true , keyboard: true});
+//         }); 
+//     });
+    
+//     // when the modal show event fires, load the url that was copied to the data-href attribute
+//     $('#modal-editUser').bind('show', function() {
+//         $(this).load($(this).attr('data-href'));
+//     });
+// });
 
 // FEATURED POSTER POSITIONING
 var featuredPosterObjs = $('.fan-art .content-featured .content');
